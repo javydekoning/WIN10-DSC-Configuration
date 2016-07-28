ec688de71520a544a79c210b58445795c81fd0ca
Javy de Koning <jdekoning@ipsoft.com>
2016-07-28 22:18:27 +0200

    Pushing W10 changes and custom resource

---
+++ ModuleTest.ps1
@@ -0,0 +1,13 @@
+﻿configuration ModuleTest
+{
+  Import-DscResource -ModuleName 'cChoco'
+  
+  node ('Localhost')
+  {   
+    cChocoInstaller choco
+    {
+      InstallDir = 'c:\testchoco\dsc\choco'
+    }
+  }
+}
+ModuleTest
