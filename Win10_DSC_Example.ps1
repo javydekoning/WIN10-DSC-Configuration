dbda145284c8871ef671f4929ccd71924973c040
javydekoning <javydekoning@gmail.com>
2016-08-05 14:48:13 +0200

    Adding Classic Shell

--- Win10_DSC_Example.ps1
+++ Win10_DSC_Example.ps1
@@ -135,7 +135,7 @@ $SystemTimeZone= 'W. Europe Standard Time'
 
 $chocopackages = 'googlechrome','filezilla','vlc','sublimetext3','jre8','7zip','greenshot',
                  'keepass','conemu','mysql.workbench','googledrive','f.lux','pidgin',
-                 'rdcman','unchecky','rufus','atom'
+                 'rdcman','unchecky','rufus','atom','classic-shell'
 
 $features      = 'Microsoft-Windows-Subsystem-Linux','Microsoft-Hyper-V','Microsoft-Hyper-V-All',
                  'Microsoft-Hyper-V-Common-Drivers-Package',
