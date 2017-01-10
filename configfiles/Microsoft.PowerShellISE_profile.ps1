3221aa0ab6e87eb4cbad7bcdbcc18324e70aaa9d
Javy de Koning <javydekoning+github@gmail.com>
2017-01-10 20:41:38 +0100

    Update Microsoft.PowerShellISE_profile.ps1
    Adding back the verbose output

--- configfiles/Microsoft.PowerShellISE_profile.ps1
+++ configfiles/Microsoft.PowerShellISE_profile.ps1
@@ -110,7 +110,8 @@ $null = $psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(
         $StringBuilder = '$host.UI.RawUI.BufferSize=new-object Management.Automation.Host.Size(320,50)',
                          '$bd=[Convert]::FromBase64String(''{0}'')',
                          '$ms=New-Object IO.MemoryStream;$ms.Write($bd,0,$bd.Length);$null=$ms.Seek(0,0)',
-                         '(New-Object IO.StreamReader((New-Object IO.Compression.GZipStream($ms,[IO.Compression.CompressionMode]0)))).ReadToEnd()|Invoke-Expression' -join ';'
+                         '(New-Object IO.StreamReader((New-Object IO.Compression.GZipStream($ms,[IO.Compression.CompressionMode]0)))).ReadToEnd()|Tee-Object -Variable exec|Write-Verbose -Verbose; iex $exec' -join ';'
         $StringBuilder -f (ConvertTo-CompressedBase64 -InputObject $psISE.CurrentFile.Editor.SelectedText) | Set-Clipboard
     }, 'CTRL+ALT+U'
 )
+
